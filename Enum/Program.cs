6d4eb9e88b76c79b44b800bd6484084475d202fe
Mehmet Sunay <mehmetsunay@windowslive.com>
2022-04-18 16:19:42 +0300

    Enum-Kullanimi
    Enum-Kullanimi

---
+++ Enum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Aldığı değeri ekrana vermek için int ekledim
+            Console.WriteLine((int)Kategoriler.Elektronik);
+            Console.ReadKey();
+           
+        }
+
+
+        //Önemli bir yeri vardır çünkü tekrar yazımları kodları engeller Hız ve verim kazandırır
+        enum Kategoriler
+        {
+            Elektronik = 3,
+            Hırdavat =2,
+            Gıda=1,
+            Güzellik=4,
+            Giyim = 5
+
+        }
+
+    }
+
+     
+}
